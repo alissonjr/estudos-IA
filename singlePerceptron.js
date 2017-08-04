193c398ff3cf165f00780bf636f67fe2d71754c2
Alisson Júnior <alisson.coj@hotmail.com>
2017-08-04 01:21:38 -0300

    inicio de documentação das funções

--- singlePerceptron.js
+++ singlePerceptron.js
@@ -6,16 +6,24 @@ class Perceptron {
 		this.learnRate = 0.15;
 		this.interactions = 1000;
 	}
-	
+	/**
+	 * @param {Number} x - 
+	 */
 	sigmoid(x) {
 		return (1 / (1 - Math.exp(-1 * x)));
 	}
 	
+	/**
+	 * @param {} learnRate - 
+	 * @param {} interactions - 
+	 */
 	init(learnRate, interactions) {
 		network.learnRate = learnRate;
 		network.interactions = interactions;
 	}
-	
+	/**
+	 * @param {Number} num - 
+	 */
 	initWeights(num) {
 		bias = parseInt(Math.random() * 10);
 		
@@ -24,6 +32,9 @@ class Perceptron {
 		}
 	}
 	
+	/**
+	 * @param {} data - 
+	 */
 	train(data) {
 		network.initWeights(data[0].inputs.length);
 		let interation = 0, error = true;
@@ -47,12 +58,19 @@ class Perceptron {
 		}
 	}
 	
+	/**
+	 * @param {Number} val - 
+	 * @param {Number} inputs - 
+	 */
 	recalcWeights(val, inputs) {
 		for(let j = 0; j < network.weights.length; j++) {
 			network.weights[j] += network.learnRate * val * inputs[j];
 		}
 	}
 	
+	/**
+	 * @param {} inputs - 
+	 */
 	run(inputs) {
 		let sum = 0;
 		for(let j = 0; j < inputs.length; j++) {
